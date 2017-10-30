1a039229ed2a0c222ba41a50c3316efe211909b5
bruno02960 <briguel1997>
2017-10-30 23:08:57 +0000

    Added missing documentation

--- TP1/Trabalho 1/handleFrames.h
+++ TP1/Trabalho 1/handleFrames.h
@@ -1,5 +1,6 @@
 #pragma once
 
+/* Reception state machine */
 typedef enum {
     START,
     FLAG_RCV,
@@ -9,11 +10,13 @@ typedef enum {
     STOP
 } ReceivingState;
 
+/* Frame responses */
 typedef enum {
     RESP_RR,
     RESP_REJ
 } FrameResponse;
 
+/* Non data frames */
 typedef enum {
     SET,
     DISC,
@@ -22,15 +25,37 @@ typedef enum {
     REJ
 } Frame;
 
+/* Frame type */
 typedef enum {
     DATA,
     NON_DATA
 } FrameType;
 
+/**
+* Writes a data frame to the serial port
+* @param data Data to be written
+* @param length Length of data
+* @param fd Serial port file descriptor
+*/
 int writeDataFrame(unsigned char* data, unsigned int length, int fd);
 
+/**
+* Receives a frame from the serial port
+* @param fSize Returns the frame size
+* @param fd Serial port file descriptor
+*/
 int receiveFrame(int* fSize, int fd);
 
+/**
+* Writes a non data frame to the serial port
+* @param frame Frame type to be written
+* @param fd Serial port file descriptor
+*/
 int writeNonDataFrame(Frame frame, int fd);
 
+/**
+* Processes a data frame received from the serial port
+* @param fResp Returns the frame response
+* @param size Frame size
+*/
 void processDataFrame(FrameResponse* fResp, int size);
