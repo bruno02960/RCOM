1941befc3f9a99f87e7271c61f7032f1a91da548
bruno02960 <briguel1997>
2017-12-09 16:40:25 +0000

    Scripts revisioned

--- TP2/new_scripts/scripts_pc2/exp4.sh
+++ TP2/new_scripts/scripts_pc2/exp4.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-route add -net 0.0.0.0 gw 172.16.11.254
+route add default gw 172.16.11.254
 #route add -net 0.0.0.0 gw 172.16.y1.254
-
-echo > 1 /proc/sys/net/ipv4/conf/all/accept redirects
-echo > 1 /proc/sys/net/ipv4/conf/eth0/accept redirects
--- TP2/new_scripts/switch.sh
+++ TP2/new_scripts/switch.sh
@@ -4,7 +4,7 @@ conf term
 no vlan 10 # no vlan y0
 no vlan 11 # no vlan y1
 end
-copy flash:tux1-clean startup-config 
+copy flash:tux1-clean startup-config
 #copy flash:gnuY-clean startup-config
 #copy flash:tuxY-clean startup-config
 reload
@@ -20,8 +20,8 @@ exit
 #interface fastethernet 0/P
 
 #For vlan y0
-interface fastethernet 0/1 
-#0/x -> port do switch range 0/1 a 0/24 
+interface fastethernet 0/1
+#0/x -> port do switch range 0/1 a 0/24
 switchport mode access
 switchport access vlan 10 #y0
 exit
@@ -50,4 +50,3 @@ end
 show running-config
 show vlan
 show interface status
-
